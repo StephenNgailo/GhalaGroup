fb740bfbe06a0558f759a2124e8bb1abbc103ca8
mmbashiti <mmbashiti@gmail.com>
2021-09-08 15:31:26 +0300

    Update models.py

--- madfox_payment_gateway/models/models.py
+++ madfox_payment_gateway/models/models.py
@@ -138,10 +138,111 @@ class PaymentAcquirerAtom(models.Model):
                           EMAIL=str(values.get ('partner_email')) ,
                           MOBILE_NO = str(values.get('partner_phone')),
                           CALL_BACK_URL=urls.url_join (base_url, '/payment/ngenius/return/'),
-                          tx_url= self.get_ngenius_urls(values),
+                          tx_url='facebook.com',
                           )
 
         return ngenius_values
+
+
+    def render(self, reference, amount, currency_id, partner_id=False, values=None):
+        """ Renders the form template of the given acquirer as a qWeb template.
+        :param string reference: the transaction reference
+        :param float amount: the amount the buyer has to pay
+        :param currency_id: currency id
+        :param dict partner_id: optional partner_id to fill values
+        :param dict values: a dictionary of values for the transction that is
+        given to the acquirer-specific method generating the form values
+
+        All templates will receive:
+
+         - acquirer: the payment.acquirer browse record
+         - user: the current user browse record
+         - currency_id: id of the transaction currency
+         - amount: amount of the transaction
+         - reference: reference of the transaction
+         - partner_*: partner-related values
+         - partner: optional partner browse record
+         - 'feedback_url': feedback URL, controler that manage answer of the acquirer (without base url) -> FIXME
+         - 'return_url': URL for coming back after payment validation (wihout base url) -> FIXME
+         - 'cancel_url': URL if the client cancels the payment -> FIXME
+         - 'error_url': URL if there is an issue with the payment -> FIXME
+         - context: Odoo context
+
+        """
+        if values is None:
+            values = {}
+
+        if not self.view_template_id:
+            return None
+
+        values.setdefault('return_url', '/payment/ngenius/return/')
+        # reference and amount
+        values.setdefault('reference', reference)
+        amount = float_round(amount, 2)
+        values.setdefault('amount', amount)
+
+        # currency id
+        currency_id = values.setdefault('currency_id', currency_id)
+        if currency_id:
+            currency = self.env['res.currency'].browse(currency_id)
+        else:
+            currency = self.env.company.currency_id
+        values['currency'] = currency
+
+        # Fill partner_* using values['partner_id'] or partner_id argument
+        partner_id = values.get('partner_id', partner_id)
+        billing_partner_id = values.get('billing_partner_id', partner_id)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            if partner_id != billing_partner_id:
+                billing_partner = self.env['res.partner'].browse(billing_partner_id)
+            else:
+                billing_partner = partner
+            values.update({
+                'partner': partner,
+                'partner_id': partner_id,
+                'partner_name': partner.name,
+                'partner_lang': partner.lang,
+                'partner_email': partner.email,
+                'partner_zip': partner.zip,
+                'partner_city': partner.city,
+
+                'partner_country': partner.country_id,
+                'partner_phone': partner.phone,
+                'partner_state': partner.state_id,
+                'billing_partner': billing_partner,
+                'billing_partner_id': billing_partner_id,
+                'billing_partner_name': billing_partner.name,
+                'billing_partner_commercial_company_name': billing_partner.commercial_company_name,
+                'billing_partner_lang': billing_partner.lang,
+                'billing_partner_email': billing_partner.email,
+                'billing_partner_zip': billing_partner.zip,
+                'billing_partner_city': billing_partner.city,
+
+                'billing_partner_country_id': billing_partner.country_id.id,
+                'billing_partner_country': billing_partner.country_id,
+                'billing_partner_phone': billing_partner.phone,
+                'billing_partner_state': billing_partner.state_id,
+            })
+        my_url=''
+        if self.provider == 'ngenius':
+            my_url = self.get_ngenius_urls(values)
+        else:
+            my_url= self._context.get('tx_url', self.get_form_action_url())
+
+        values.update({
+            'tx_url': my_url,
+            'submit_class': self._context.get('submit_class', 'btn btn-link'),
+            'submit_txt': self._context.get('submit_txt'),
+            'acquirer': self,
+            'user': self.env.user,
+            'context': self._context,
+            'type': 'get',
+           # 'type': values.get('type') or 'form',
+        })
+
+        return self.view_template_id._render(values, engine='ir.qweb')
+
 
 class PaymentTransactionAtom(models.Model):
     _inherit = 'payment.transaction'
